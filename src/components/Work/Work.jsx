a73d2c3ed61501448f9541940fd717996d2f6d41
Teller35 <tellerwetzel@yahoo.com>
2021-07-28 13:26:06 -0600

    started work on project page

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Work() {
+  const [projects] = useState([
+    {
+      name: "Med Planner",
+      description:
+        "An app that help patients schedule appointments with doctors and help keep track of them.",
+      url: "https://med-planner.herokuapp.com/",
+      github: "https://github.com/Teller35/med-planner",
+      image: "med-planner.png",
+    },
+    {},
+    {},
+    {},
+    {},
+    {},
+  ]);
   return (
     <section>
-      <h1>Portfolio</h1>
-      
+      {projects.map((project) => (
+        <div>
+          <span>{project.name}</span>
+          <img
+            src={require(`../../assets/large/${project.image}`).default}
+            alt={project.name}
+          />
+          <div>
+            <p>{project.description}</p>
+            <a href={project.url}>Live {project.name}</a>
+            <a href={project.github}>GitHub {project.name}</a>
+          </div>
+        </div>
+      ))}
     </section>
   );
 }
