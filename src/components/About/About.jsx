a9e4eada67e9355f9f864b10bc87f7e179ee1af7
Teller35 <tellerwetzel@yahoo.com>
2021-07-29 10:17:37 -0600

    fixed alt tag

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,7 @@ function About() {
   return (
     <section>
       <h1 className="Second">About Me:</h1>
-      <img src={coverImage} alt="Me at the zoo" className="Items CoverImg" />
+      <img src={coverImage} alt="Me at the park" className="Items CoverImg" />
       <div className="Items">
         <p className="First">
         Full-Stack web developer who is always looking to increase my knowledge base. I have recently earned my certificate in Full-Stack web development from U of U Coding Bootcamp. I have learned skills in JavaScript, jQuery, CSS, SQL, NoSQL, and React. Have developed applications with my fellow cohorts. Have always been a quick learner, very hands on, and not afraid to tackle hard problems.
