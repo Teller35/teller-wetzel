0f663985c84730cfa1c05b54b319efbde77e7db7
Teller35 <tellerwetzel@yahoo.com>
2021-08-03 09:56:03 -0600

    added email icon and link to page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { SiGithub } from "react-icons/si";
 import { SiLinkedin } from "react-icons/si";
 import { SiTwitter } from "react-icons/si";
+import { SiMailDotRu } from "react-icons/si";
 
 function Footer() {
   return (
@@ -10,6 +11,7 @@ function Footer() {
         <a className="Links Items col-auto" href="https://github.com/Teller35" target="_blank"><SiGithub size={20}/></a>
         <a className="Links Items col-auto" href="https://www.linkedin.com/in/tellerwetzel/" target="_blank"><SiLinkedin size={20}/></a>
         <a className="Links Items col-auto" href="https://twitter.com/TellerWetzel" target="_blank"><SiTwitter size={20}/></a>
+        <a className="Links Items col-auto" href="mailto:tellerwetzel@yahoo.com"><SiMailDotRu size={20}/></a>
       </div>
     </footer>
   );
